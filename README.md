42e59650c5b143766418cbd1c9eb0fc187618d9e
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-10-14 04:42:44 +0300

    add frontend

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FullStackOpenPart3
-FullStackOpen part 3 backend exercises
+Links to my Heroku apps:
+
+Backend (3.10): https://polar-crag-48735.herokuapp.com/api/persons
+
+Frontend+Backend(3.11): 
---
+++ build/precache-manifest.67d6f45b767ba13983276bf0ce070645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3a8c841dd6bc6ec37e495051b45122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0933af55fe2aba81e14a",
+    "url": "/static/css/main.73990ae2.chunk.css"
+  },
+  {
+    "revision": "d3f96efc87a8162e5c1d",
+    "url": "/static/js/2.79f048fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79f048fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0933af55fe2aba81e14a",
+    "url": "/static/js/main.aa4c29b2.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
